2521a46dc72aedb2a9fe0a0a0ca934965a5c95b1
Basiru Danso <basirudns@gmail.com>
2019-05-01 06:04:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,14 +6,14 @@
  # Array = []
 #end
 
-def add_element_to_end_of_array(array, element)
-  array.each_with_index do|array element|.push
-  
-end
-
-#def add_element_to_start_of_array(array, element)
+#def add_element_to_end_of_array(array, element)
+  #array.each_with_index do|array element|.push
   
 #end
+
+def add_element_to_start_of_array(array, element)
+  
+end
 
 #def remove_element_from_end_of_array(array)
   
